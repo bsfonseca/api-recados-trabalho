577a380a31be75bcb9e9e01e60b315098c334981
bsfonseca <bs.fonseca12@gmail.com>
2023-08-01 18:24:59 -0300

    Rotas para criar e listar usuários

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { usuarios } from "./usuarios.js";
+import { usuarios, criarUsuario } from "./usuarios.js";
 
 const app = express();
 app.use(express.json());
@@ -17,6 +17,7 @@ app.get("/usuarios", (req, res) => {
 app.post("/usuarios", (req, res) => {
   const { nome, email, senha } = req.body;
 
+  // Validações de campo obrigatório
   if (!nome) {
     return res.status(400).send({
       ok: false,
@@ -38,7 +39,19 @@ app.post("/usuarios", (req, res) => {
     });
   }
 
-  usuarios.push({
+  // Validação de email já existente
+  const user = usuarios.find((usuario) => usuario.email == email);
+
+  // Existe um usuario com o email informado
+  if (user != undefined) {
+    // retornar erro
+    return res.status(400).send({
+      ok: false,
+      mensagem: "Email ja cadastrado",
+    });
+  }
+
+  criarUsuario({
     nome,
     email,
     senha,
@@ -46,7 +59,59 @@ app.post("/usuarios", (req, res) => {
 
   res.status(201).send({
     ok: true,
-    mensagem: "Teste",
+    mensagem: "Usuário criado com sucesso",
+    dados: usuarios,
+  });
+});
+
+app.post("/login", (req, res) => {
+  const { email, senha } = req.body;
+
+  if (!email) {
+    return res.status(400).send({
+      ok: false,
+      mensagem: "Email não informado",
+    });
+  }
+
+  if (!senha) {
+    return res.status(400).send({
+      ok: false,
+      mensagem: "Senha não informada",
+    });
+  }
+
+  const login = usuarios.find((user) => user.email == email && user.senha == senha);
+
+  if (login == undefined) {
+    return res.status(400).send({
+      ok: false,
+      mensagem: "Erro no login",
+    });
+  }
+
+  res.status(200).send({
+    ok: true,
+    mensagem: "Login realizado com sucesso",
+  });
+});
+
+app.get("/usuarios/:id/recados", (req, res) => {
+  const { id } = req.params;
+
+  const user = usuarios.find((user) => user.id == id);
+  // se user nao existe, retorna erro 404
+  if (user == undefined) {
+    return res.status(404).send({
+      ok: false,
+      mensagem: "Recado não encontrado para o usuário",
+    });
+  }
+  // retorna sucesso, pega os recados dentro de user.recados
+  res.status(200).send({
+    ok: true,
+    mensagem: "Recado retornado com sucesso",
+    dado: user.recados,
   });
 });
 
--- src/usuarios.js
+++ src/usuarios.js
@@ -1,17 +1,33 @@
 export const usuarios = [
   {
+    id: 1,
     nome: "João",
     email: "joao@gmail.com",
     senha: 12345,
+    recados: ["Descontos para compras acima de 100 reais"],
   },
   {
+    id: 2,
     nome: "Daphne",
     email: "daphne@gmail.com",
     senha: "teste123",
+    recados: ["Retire seu brinde até dia 30/07/2023"],
   },
   {
+    id: 3,
+
     nome: "Paulo",
     email: "paulo@gmail.com",
-    senha: "usario456",
+    senha: "usuario456",
+    recados: ["Frete grátis em toda loja"],
   },
 ];
+
+let contador = usuarios.length + 1;
+
+export function criarUsuario(usuario) {
+  usuario.id = contador;
+  contador++;
+
+  usuarios.push(usuario);
+}
