82d5afb073830eb6003c5e0b4dff9c1b984770d7
bsfonseca <bs.fonseca12@gmail.com>
2023-08-01 11:37:42 -0300

    Lista de usuários para testes

---
+++ src/usuarios.js
@@ -0,0 +1,26 @@
+export const usuarios = [
+  {
+    nome: "João",
+    idade: 50,
+    email: "joao@gmail.com",
+    endereco: "Rua Voluntários da Pátria",
+    cidade: "Rosário do sul",
+    estado: "Rio Grande do Sul",
+  },
+  {
+    nome: "Daphne",
+    idade: 18,
+    email: "daphne@gmail.com",
+    endereco: "Rua Piauí",
+    cidade: "Porto Alegre",
+    estado: "Rio Grande do Sul",
+  },
+  {
+    nome: "Paulo",
+    idade: 30,
+    email: "paulo@gmail.com",
+    endereco: "Rua Piauí",
+    cidade: "Porto Alegre",
+    estado: "Rio Grande do Sul",
+  },
+];
